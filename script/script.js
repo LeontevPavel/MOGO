3b728441e36656660019de30a45bca08d7823dba
LeontevPavel <leontev@mail.ru>
2023-09-26 23:25:13 +0500

    Adaptiv and slider

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+// ---------- Подключение яндекс-карт -----------//
 function init() {
     let myMap = new ymaps.Map("map", {
         center: [56.841248691452755,60.59780669291867],
@@ -14,3 +15,52 @@ myMap.geoObjects.add(myPlacemark);
 }
 
 ymaps.ready(init);
+
+
+// --------- Галерея комментариев ---------------//
+
+const rightButtons = document.querySelectorAll('.btn-right');       
+const leftButtons = document.querySelectorAll('.btn-left'); 
+let i = 0;
+
+[...rightButtons].forEach(rightButton => {
+rightButton.onclick = () => {  
+    const itemCollection = Array.from(rightButton.parentElement.querySelectorAll('.comment-item'));    
+    console.log(itemCollection);
+    if (i+1 === itemCollection.length){
+        i = -1;
+        itemCollection[itemCollection.length - 1].classList.remove("item-active");
+    } 
+    i++;
+ removeRight(itemCollection)
+}});
+
+[...leftButtons].forEach(leftButton => {
+leftButton.onclick = () => {
+    const itemCollection = Array.from(leftButton.parentElement.querySelectorAll('.comment-item')); 
+    if (i === 0){
+        i = itemCollection.length;
+        itemCollection[0].classList.remove("item-active");
+    } 
+    i--;
+    removeLeft(itemCollection)
+}});
+
+function removeRight(itemCollection) {    
+    itemCollection[i].classList.add("item-active")
+    if(i === 0) {
+        itemCollection[itemCollection.length - 1].classList.remove("item-active")
+    } else {
+        itemCollection[i-1].classList.remove("item-active") 
+    }       
+}
+
+function removeLeft(itemCollection) {   
+    itemCollection[i].classList.add("item-active")
+    if(i === itemCollection.length - 1) {
+        itemCollection[0].classList.remove("item-active")  
+    } else {
+        itemCollection[i+1].classList.remove("item-active")
+    }
+          
+}   
